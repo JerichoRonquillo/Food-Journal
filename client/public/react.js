b70fae6f7edef0dd80382de4ed8713d82d5c488d
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-11-20 13:50:06 -0800

    Edited

--- client/public/react.js
+++ client/public/react.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+// Set up the title/header
 const element = (
     <div>
         <header>
@@ -9,17 +10,23 @@ const element = (
 )
 ReactDOM.render(element, document.getElementById('root'));
 
+// useState will take in the users inputted food 
 function foodForm() {
-    const [food, setFood] = useState;
-    
-    return (
-        <form>
-            <label>What did you eat today?
-                <input type="text"></input>
-                <button onClick={() => setFood(food)}>
-                    Submit
-                </button>
-            </label>
-        </form>
-    )
+    const [food, setFood] = useState('');
+
+    // Allows for the inputted food to be moved over to <p> tag
+    useEffect(() => {
+        document.title = `Today you ate:${food}`;
+    })
 }
+// Once the button is clicked the submitted food object will be sent to a seperate container of listed foods for that day...Hopefully
+return (
+    <form>
+        <label>What did you eat today?
+            <input type="text"></input>
+            <button onClick={() => setFood(food)}>
+                Submit
+            </button>
+        </label>
+    </form>
+)
